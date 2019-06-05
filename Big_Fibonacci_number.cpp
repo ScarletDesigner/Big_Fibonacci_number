642871d3889bbfc9bbebacb9330794209a7b9907
ScarletDesigner <50834371+ScarletDesigner@users.noreply.github.com>
2019-06-05 22:26:14 +0200

    Removed some redundant objects

--- Big_Fibonacci_number.cpp
+++ Big_Fibonacci_number.cpp
@@ -1,100 +1,113 @@
 #include <iostream>
 using namespace std;
-int isShorter(string a, string b)
-{
-	if (a.length() < b.length())
-		return a.length();
-	else
-		return b.length();
-}
 
 class AttributesToAddStrings
 {
-	public:
-		string first, second;
-		int shorterLength;
-		int firstLeftDigits, secondLeftDigits;
-		string result;
-		int shift;
-		int lastDigit;
-		static const int ASCIofZero = 48;
+public:
+	string first, second;
+	int shorterLength;
+	int firstLeftDigits, secondLeftDigits;
+	string result;
+	int shift;
+	int lastDigit;
+	static const int ASCIofZero = 48;
 
-		initialization(string one, string two)
+	initialization(string one, string two)
+	{
+		first = one;
+		second = two;
+		shorterLength = isShorter(one, two);
+		firstLeftDigits = one.length();
+		secondLeftDigits = two.length();
+		result = "";
+		shift = 0;
+	}
+	
+	string fibonacci(int n)
+	{
+		if(n == 0)
+			return "0";
+		if(n == 1)
+			return "1";
+			
+		string fib[n+1];
+		fib[0] = '0';
+		fib[1] = '1';
+		for(int i=2; i<=n; i++)
 		{
-			first = one;
-			second = two;
-			shorterLength = isShorter(one, two);
-			firstLeftDigits = one.length();
-			secondLeftDigits = two.length();
-			result = "";
-			shift = 0;
+			fib[i] = add(fib[i-1], fib[i-2]);
 		}
+		
+		return fib[n];
+	}
+	
+	string add(string first, string second)
+	{
+		initialization(first, second);
+		addTillLengthMatches();
+		addLeftDigitsOfFirstString();
+		addLeftDigitsOfSecondString();
+		addLeftShiftToNewRow();
+		checkForEmptyResult();
+		return result;
+	}
+	
+	int isShorter(string a, string b)
+	{
+		if (a.length() < b.length())
+			return a.length();
+		else
+			return b.length();
+	}
+	
+	void addTillLengthMatches()
+	{
+		for(int i=1; i <= shorterLength; i++)
+		{
+			lastDigit = (int)(first[--firstLeftDigits]) + (int)(second[--secondLeftDigits]) + shift - 96;
+			shift = lastDigit / 10;
+			result = (char)((lastDigit % 10) + ASCIofZero) + result;
+		}
+	}
+	
+	void addLeftDigitsOfFirstString()
+	{
+		while(firstLeftDigits > 0)
+		{
+			lastDigit = first[--firstLeftDigits] + shift - ASCIofZero;
+			shift = lastDigit / 10;
+			result = (char)((lastDigit % 10) + ASCIofZero) + result;
+		}
+	}
+	
+	void addLeftDigitsOfSecondString()
+	{
+		while(secondLeftDigits > 0)
+		{
+			lastDigit = second[--secondLeftDigits] + shift - 48;
+			shift = lastDigit / 10;
+			result = (char)((lastDigit % 10) + 48) + result;
+		}
+	}
+	
+	void addLeftShiftToNewRow()
+	{
+		if(shift > 0)
+			result = (char)(shift + 48) + result;
+	}
+	
+	void checkForEmptyResult()
+	{
+		if(result == "")
+			result = "0";
+	}
+	
 };
-void addTillLengthMatches(AttributesToAddStrings &a)
-{
-	for(int i=1; i <= a.shorterLength; i++)
-	{
-		a.lastDigit = (int)(a.first[--a.firstLeftDigits]) + (int)(a.second[--a.secondLeftDigits]) + a.shift - 96;
-		a.shift = a.lastDigit / 10;
-		a.result = (char)((a.lastDigit % 10) + a.ASCIofZero) + a.result;
-	}
-}
-void addLeftDigitsOfFirstString(AttributesToAddStrings &a)
-{
-	while(a.firstLeftDigits > 0)
-	{
-		a.lastDigit = a.first[--a.firstLeftDigits] + a.shift - a.ASCIofZero;
-		a.shift = a.lastDigit / 10;
-		a.result = (char)((a.lastDigit % 10) + a.ASCIofZero) + a.result;
-	}
-}
-void addLeftDigitsOfSecondString(AttributesToAddStrings &a)
-{
-	while(a.secondLeftDigits > 0)
-	{
-		a.lastDigit = a.second[--a.secondLeftDigits] + a.shift - 48;
-		a.shift = a.lastDigit / 10;
-		a.result = (char)((a.lastDigit % 10) + 48) + a.result;
-	}
-}
-void addLeftShiftToNewRow(AttributesToAddStrings &a)
-{
-	if(a.shift > 0)
-		a.result = (char)(a.shift + 48) + a.result;
-}
-void checkForEmptyResult(AttributesToAddStrings &a)
-{
-	if(a.result == "")
-		a.result = "0";
-}
-string add(string first, string second)
-{
-	AttributesToAddStrings a;
-	a.initialization(first, second);
-	addTillLengthMatches(a);
-	addLeftDigitsOfFirstString(a);
-	addLeftDigitsOfSecondString(a);
-	addLeftShiftToNewRow(a);
-	checkForEmptyResult(a);
-	return a.result;
-}
 
-string fibonacci(int n)
-{
-	if(n == 0)
-		return "0";
-	if(n == 1)
-		return "1";
-	string fib[n+1];
-	fib[0] = '0';
-	fib[1] = '1';
-	for(int i=2; i<=n; i++)
-	{
-		fib[i] = add(fib[i-1], fib[i-2]);
-	}
-	return fib[n];
-}
 int main(){
-cout<<fibonacci(1000);
+	
+	AttributesToAddStrings a;
+	cout<<a.fibonacci(1000);
+	
 	return 0;
 }
